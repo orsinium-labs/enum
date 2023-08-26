ef23cf0b14dcb1c743f6e93559e7fcbf6a300e97
gram <git@orsinium.dev>
2023-08-26 20:13:55 +0200

    Enum.Values

--- enum.go
+++ enum.go
@@ -1,8 +1,9 @@
 package enum
 
-type Enum[T comparable] struct {
-	members []Member[T]
-}
+import (
+	"fmt"
+	"strings"
+)
 
 type Member[T comparable] struct {
 	value T
@@ -12,8 +13,42 @@ func M[T comparable](value T) Member[T] {
 	return Member[T]{value}
 }
 
+func (m Member[T]) Value() T {
+	return m.value
+}
+
+func (m Member[T]) TypeName() string {
+	// The %T format returns the type name containing
+	// the module name in which it is defined.
+	// However, enum members are usually defined
+	// right next to the type they wrap,
+	// so we can drop the package name.
+	name := fmt.Sprintf("%T", *new(T))
+	parts := strings.Split(name, ".")
+	if len(parts) > 1 {
+		return parts[len(parts)-1]
+	}
+	return parts[0]
+}
+
+func (m Member[T]) String() string {
+	return fmt.Sprintf("%v", m.value)
+}
+
+func (m Member[T]) GoString() string {
+	return fmt.Sprintf("enum.M[%s](%#v)", m.TypeName(), m.value)
+}
+
+type Enum[T comparable] struct {
+	members []Member[T]
+}
+
 func New[T comparable](members ...Member[T]) Enum[T] {
 	return Enum[T]{members}
+}
+
+func (m Enum[T]) TypeName() string {
+	return Member[T]{}.TypeName()
 }
 
 func (e Enum[T]) Empty() bool {
@@ -57,4 +92,12 @@ func (e Enum[T]) Index(member Member[T]) int {
 
 func (e Enum[T]) Members() []Member[T] {
 	return e.members
+}
+
+func (e Enum[T]) Values() []T {
+	res := make([]T, len(e.members))
+	for _, m := range e.members {
+		res = append(res, m.value)
+	}
+	return res
 }
--- enum_test.go
+++ enum_test.go
@@ -1,21 +1,50 @@
 package enum_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/matryer/is"
 	"github.com/orsinium-labs/enum"
 )
 
-func TestEnumSmoke(t *testing.T) {
+type Color string
+
+var (
+	Red    = enum.M[Color]("red")
+	Green  = enum.M[Color]("green")
+	Blue   = enum.M[Color]("blue")
+	Colors = enum.New(Red, Green, Blue)
+)
+
+func TestInterfaces(t *testing.T) {
+	var _ fmt.Stringer = enum.Member[any]{}
+	var _ fmt.GoStringer = enum.Member[any]{}
+}
+
+func TestMemberValue(t *testing.T) {
 	is := is.New(t)
-	type Color string
-	var (
-		Red    = enum.M[Color]("red")
-		Green  = enum.M[Color]("green")
-		Blue   = enum.M[Color]("blue")
-		Colors = enum.New(Red, Green, Blue)
-	)
+	is.Equal(Red.Value(), Color("red"))
+	is.Equal(Green.Value(), Color("green"))
+	is.Equal(Blue.Value(), Color("blue"))
+	is.Equal(enum.M[string]("blue").Value(), "blue")
+	is.Equal(enum.M[int](14).Value(), 14)
+}
+
+func TestMemberString(t *testing.T) {
+	is := is.New(t)
+	act := fmt.Sprint(Red)
+	is.Equal(act, "red")
+}
+
+func TestMemberGoString(t *testing.T) {
+	is := is.New(t)
+	act := fmt.Sprintf("%#v", Red)
+	is.Equal(act, `enum.M[Color]("red")`)
+}
+
+func TestEnumParse(t *testing.T) {
+	is := is.New(t)
 	parsed := Colors.Parse("red")
-	is.Equal(parsed, Red)
+	is.Equal(parsed, &Red)
 }
