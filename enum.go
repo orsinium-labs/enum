7ff4f9158b2202af1cd6e21562fe96319b735cf2
gram <git@orsinium.dev>
2023-08-29 09:04:01 +0200

    rename type params

--- enum.go
+++ enum.go
@@ -27,7 +27,7 @@ func (m Member[T]) GetValue() T {
 }
 
 // TypeName is a string representation of the wrapped type.
-func (m Member[T]) TypeName() string {
+func (Member[T]) TypeName() string {
 	return fmt.Sprintf("%T", *new(T))
 }
 
@@ -53,32 +53,32 @@ func (m Member[T]) GoString() string {
 // Enum is a collection of enum members.
 //
 // Use [New] to construct a new Enum from a list of members.
-type Enum[M iMember[T], T comparable] struct {
+type Enum[M iMember[V], V comparable] struct {
 	members []M
 }
 
 // New constructs a new [Enum] wrapping the given enum members.
-func New[T comparable, M iMember[T]](members ...M) Enum[M, T] {
-	return Enum[M, T]{members}
+func New[V comparable, M iMember[V]](members ...M) Enum[M, V] {
+	return Enum[M, V]{members}
 }
 
 // TypeName is a string representation of the wrapped type.
-func (m Enum[M, T]) TypeName() string {
-	return fmt.Sprintf("%T", *new(T))
+func (Enum[M, V]) TypeName() string {
+	return fmt.Sprintf("%T", *new(V))
 }
 
 // Empty returns true if the enum doesn't have any members.
-func (e Enum[M, T]) Empty() bool {
+func (e Enum[M, V]) Empty() bool {
 	return len(e.members) == 0
 }
 
 // Len returns how many mambers the enum has.
-func (e Enum[M, T]) Len() int {
+func (e Enum[M, V]) Len() int {
 	return len(e.members)
 }
 
 // Contains returns true if the enum has the given member.
-func (e Enum[M, T]) Contains(member M) bool {
+func (e Enum[M, V]) Contains(member M) bool {
 	for _, m := range e.members {
 		if m.GetValue() == member.GetValue() {
 			return true
@@ -90,7 +90,7 @@ func (e Enum[M, T]) Contains(member M) bool {
 // Parse converts a raw value into a member of the enum.
 //
 // If none of the enum members has the given value, nil is returned.
-func (e Enum[M, T]) Parse(value T) *M {
+func (e Enum[M, V]) Parse(value V) *M {
 	for _, member := range e.members {
 		if member.GetValue() == value {
 			return &member
@@ -100,14 +100,14 @@ func (e Enum[M, T]) Parse(value T) *M {
 }
 
 // Value returns Member.Value of the given enum member.
-func (e Enum[M, T]) Value(member M) T {
+func (Enum[M, V]) Value(member M) V {
 	return member.GetValue()
 }
 
 // Index returns the index of the given member in the enum.
 //
 // If the given memeber is not in the enum, it panics.
-func (e Enum[M, T]) Index(member M) int {
+func (e Enum[M, V]) Index(member M) int {
 	for i, m := range e.members {
 		if m.GetValue() == member.GetValue() {
 			return i
@@ -117,13 +117,13 @@ func (e Enum[M, T]) Index(member M) int {
 }
 
 // Members returns a slice of the members in the enum.
-func (e Enum[M, T]) Members() []M {
+func (e Enum[M, V]) Members() []M {
 	return e.members
 }
 
 // Values returns a slice of values of all memebers of the enum.
-func (e Enum[M, T]) Values() []T {
-	res := make([]T, 0, len(e.members))
+func (e Enum[M, V]) Values() []V {
+	res := make([]V, 0, len(e.members))
 	for _, m := range e.members {
 		res = append(res, m.GetValue())
 	}
