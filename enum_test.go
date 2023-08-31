325f92bdae0fa818e8903c603c39be4f3bdcc041
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-31 13:50:38 +0200

    Make one test a bit shorter

--- enum_test.go
+++ enum_test.go
@@ -83,8 +83,7 @@ func TestEnum_Choice(t *testing.T) {
 	is.Equal(*m, Blue)
 	// Check that selecting a random member from an empty Enum returns an error
 	emptyEnums := enum.New[string, Color]()
-	m = emptyEnums.Choice(0)
-	is.True(m == nil)
+	is.True(emptyEnums.Choice(0) == nil)
 }
 
 func TestEnum_Values(t *testing.T) {
